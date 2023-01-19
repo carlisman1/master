5106a37554549d0989bf29e77eae9d4817486ebc
juancarlosmaranon <juancar2001jcmg@gmail.com>
2023-01-19 14:22:55 +0100

    dsf

--- CSharp/Fundamentos/ClassLibrary1/Temperatura.cs
+++ CSharp/Fundamentos/ClassLibrary1/Temperatura.cs
@@ -6,9 +6,8 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
-    public class Temperaturas
+    public class Temperatura
     {
-        public int Temperatura { get; set; }
         public int Maxima { get; set; }
         public int Minima { get; set; }
         public int Media { get; set; }
